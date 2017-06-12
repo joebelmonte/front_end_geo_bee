5ec0a5fd0b6429b152be0d6fca87f4fb6a91d3cc
joebelmonte <joseph.belmonte@gmail.com>
2017-06-12 13:37:00 -0400

    Added skeleton for game-play handlebars
    Need to add the map next

--- assets/scripts/game-play/events.js
+++ assets/scripts/game-play/events.js
@@ -3,10 +3,11 @@ const getFormFields = require(`../../../lib/get-form-fields`)
 const api = require('./api')
 const ui = require('./ui')
 const store = require('../store.js')
+const gamePlay = require('../templates/game-play.handlebars')
 
 const onStartNewGame = function (event) {
   event.preventDefault()
-  console.log('in onStartNewGame')
+  $('#game-state-container').html(gamePlay)
 }
 
 const addGameHandlers = () => {
